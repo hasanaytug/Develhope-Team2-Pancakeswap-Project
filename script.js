4f08864f4ae56ece867a4cab551eef495a08bb58
Antonio Pons <toninopons@gmail.com>
2022-11-21 05:41:53 +0100

    Added a bit more of functionality to infos-1 section.

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 /* START of INFOS */
 const infosTitle = ['Farms', 'Syrup Pools'];
+
 const dataBoxTitles = {
     'Farms' : ['TWT-BNB LP', 'KRS-BUSD LP', 'XCAD-BUSD LP', 'CO-BUSD LP', 'ARV-BNB LP'],
     'Syrup Pools' : ['Stake CAKE', 'Stake CAKE - Earn HOOP', 'Stake CAKE - Earn CO', 'Stake CAKE - Earn KRS', 'Stake CAKE - Earn MGP']
-}
+};
+
+const dataBoxPercentages = {
+    'Farms' : ['199.050%', '188.301%', '179.283%', '162.717%', '153.702%'],
+    'Syrup Pools' : ['59.200%', '220.422%', '175.543%', '169.051%', '133.768%']
+};
 
 let categoryIndex = 0;
 let dataBoxIndex = 0;
 
 document.querySelectorAll('.data-box > h3').forEach(el => {
     el.textContent = dataBoxTitles[infosTitle[categoryIndex]][dataBoxIndex++];
+});
+
+dataBoxIndex = 0;
+document.querySelectorAll('#percentage').forEach(el => {
+    el.textContent = dataBoxPercentages[infosTitle[categoryIndex]][dataBoxIndex++];
 });
 
 document.querySelector('#up-down-button').addEventListener('click', () => {
@@ -20,5 +31,11 @@ document.querySelector('#up-down-button').addEventListener('click', () => {
     document.querySelectorAll('.data-box > h3').forEach(el => {
         el.textContent = dataBoxTitles[currentCategory][dataBoxIndex++ % dataBoxTitles[currentCategory].length];
     });
+
+    document.querySelectorAll('#percentage').forEach(el => {
+        el.textContent = dataBoxPercentages[currentCategory][dataBoxIndex++ % dataBoxTitles[currentCategory].length];
+    })
 });
+
+
 /* END of INFOS */
