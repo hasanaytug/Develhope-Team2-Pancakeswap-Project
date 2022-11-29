097932934aca61a9147a2a5519cbc7f959744286
Antonio Pons <toninopons@gmail.com>
2022-11-29 08:34:46 +0100

    Changed open-in-new icon for an svg. Added header pagination swippers, button hover effect.

--- navbarAndPhishingScroll.js
+++ navbarAndPhishingScroll.js
@@ -1,20 +1,17 @@
 /* START of NAVBAR section*/
 window.onscroll = hideNav;
 
-const phishing = document.querySelector('#phishing-warning');
-const navbar = document.querySelector('.navbar');
-
 function hideNav() {
-    const phishingWarningVisible = phishing.style.display !== 'none';
+    const phishingWarningVisible = phishingWarning.style.display !== 'none';
     if (document.body.scrollTop > 500 
         || document.documentElement.scrollTop > 500) {
             if (phishingWarningVisible)
-                phishing.style.top = '-100px';
+                phishingWarning.style.top = '-100px';
         navbar.style.top = '-100px';
         
     } else {
         if (phishingWarningVisible) {
-            phishing.style.top = '0';
+            phishingWarning.style.top = '0px';
             navbar.style.top = '70px';
         } else {
             navbar.style.top = '0px';
--- phishing-warning.js
+++ phishing-warning.js
@@ -1,10 +1,10 @@
 /* START of PHISHING-WARNING */
 const phishingWarning = document.querySelector('#phishing-warning');
-setTimeout(() => phishingWarning.style.display = 'flex', 3000);
+const navbar = document.querySelector('.navbar');
 
 const phishingButton = document.querySelector('#phishing-warning > button');
 phishingButton.addEventListener('click', () => {
   phishingWarning.style.display = 'none';
-  document.querySelector('.navbar').style.top = '0';
+  document.querySelector('.navbar').style.top = '0px';
 });
 /* END of PHISHIN-WARNING */
