d7b24b2a1a54ae59833ebf4caba938ba08da91e0
İbrahim Uslu <ibrahim069612@gmail.com>
2022-11-29 16:37:37 +0300

    head of the middle part completed as skeleton

---
+++ script.js
@@ -0,0 +1,19 @@
+// 2 Way-Switches Toggle Start
+
+$(document).ready(function () {
+  $("#color_mode").on("change", function () {
+    colorModePreview(this);
+  });
+});
+
+function colorModePreview(ele) {
+  if ($(ele).prop("checked") == true) {
+    $("body").addClass("dark-preview");
+    $("body").removeClass("white-preview");
+  } else if ($(ele).prop("checked") == false) {
+    $("body").addClass("white-preview");
+    $("body").removeClass("dark-preview");
+  }
+}
+
+// 2 Way-Switches Toggle End
