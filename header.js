b523f1e7d7b94c61cac6b2a2ba247bb3fead7f83
Antonio Pons <toninopons@gmail.com>
2022-11-30 05:56:46 +0100

    javascript: changing background linear gradient and header box tag in relation to swipper bullet selected, done.

--- header.js
+++ header.js
@@ -1,9 +1,21 @@
+const arrayFromNodeList = (arr) => Array.from(arr);
+
 const swipperBullets = document.querySelectorAll('.header-swipper');
 const active = 'header-bullet-active';
+
 const headerButton = document.querySelector('.ibrahim-button');
-const buttonChildren = Array.from(headerButton.querySelectorAll('span'));
+const buttonChildren = 
+        arrayFromNodeList(headerButton.querySelectorAll('span'));
+
 const headerBox = document.querySelector('.ibrahim-rectangle');
-const headerBoxImages = Array.from(headerBox.querySelectorAll('.header-box-imgs'));
+const headerBoxImages = 
+        arrayFromNodeList(headerBox.querySelectorAll('.header-box-imgs'));
+
+const headerBoxTagChildren = document.querySelectorAll('#headTag h1, #header-counter');
+const headerBoxTagChildrenArr = arrayFromNodeList(headerBoxTagChildren);
+
+const headerBoxContainer = document.querySelector('.ibrahim-rectangle');
+
 
 swipperBullets.forEach((bullet) => bullet.addEventListener('click', () => {
     if (!bullet.hasAttribute('id')) {
@@ -14,20 +26,37 @@ swipperBullets.forEach((bullet) => bullet.addEventListener('click', () => {
 
         if (selectedBullet >= 0 && selectedBullet < buttonChildren.length) {
             buttonChildren[selectedBullet].style.display = 'flex';
-            headerBoxImages[selectedBullet].style.display = 'flex';            
+            headerBoxImages[selectedBullet].style.display = 'flex'; 
+            headerBoxTagChildren[selectedBullet].style.display = 'flex';
+
+            /*
+            selectedBullet === 0 
+                ? headerBoxContainer.style.background 
+                = 'linear-gradient(rgb(0, 191, 165) 0%, rgb(0, 90, 90) 100%);'
+                : 'linear-gradient(rgb(115, 67, 211) 0%, rgb(72, 44, 128) 100%);';
+            */
+            if (selectedBullet === 0)
+                headerBoxContainer.style.background = 'linear-gradient(rgb(0, 191, 165) 0%, rgb(0, 90, 90) 100%)';
+            else
+                headerBoxContainer.style.background = 'linear-gradient(rgb(115, 67, 211) 0%, rgb(72, 44, 128) 100%)';
+
+            console.log(headerBoxContainer.style.background);
         }
 
-        buttonChildren.map(el => {
-            if (buttonChildren.indexOf(el) !== selectedBullet)
-                el.style.display = 'none';
-        });
+        hideUnselectedElements(buttonChildren, selectedBullet);
+        hideUnselectedElements(headerBoxImages, selectedBullet);
+        hideUnselectedElements(headerBoxTagChildrenArr, selectedBullet);
 
-        headerBoxImages.map(el => {
-            if (headerBoxImages.indexOf(el) !== selectedBullet)
-                el.style.display = 'none';
-        });
+
         
     }
-}))
+}));
 
-console.log(swipperBullets);
+
+const hideUnselectedElements = (itemsArr, selected) => {
+    return itemsArr.map(el => {
+        if (itemsArr.indexOf(el) !== selected)
+            el.style.display = 'none';
+    });
+}
+
