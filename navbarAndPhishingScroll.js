a904a7736c2b229eb51a3b65148e91b257003fdd
Antonio Pons <toninopons@gmail.com>
2022-12-01 08:42:08 +0100

    Created 2 different classes to position warning and navbar differently when onscroll effect gets triggered. Also since the scroll effect script is not modifying top property manually, the warning in the console went away.

--- navbarAndPhishingScroll.js
+++ navbarAndPhishingScroll.js
@@ -5,11 +5,11 @@ function hideNav() {
     const phishingWarningVisible = phishingWarning.style.display !== 'none';
     if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
             if (phishingWarningVisible)
-                phishingWarning.classList.add('hide-onscroll');
-        navbar.classList.add('hide-onscroll');
+                phishingWarning.classList.add('hide-onscroll-warning');
+        navbar.classList.add('hide-onscroll-navbar');
     } else {
-        navbar.classList.remove('hide-onscroll');
-        phishingWarning.classList.remove('hide-onscroll');
+        navbar.classList.remove('hide-onscroll-navbar');
+        phishingWarning.classList.remove('hide-onscroll-warning');
     }
 };
 /* end of NAVBAR section*/
