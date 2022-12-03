7a6a3d30e47612ecff83f1bae4035a22371e4545
Antonio Pons <toninopons@gmail.com>
2022-12-03 19:09:13 +0100

    navbar-right, network selection done.

--- navbar.js
+++ navbar.js
@@ -1,6 +1,32 @@
-let netSelectionOptions = Array.from(document.querySelector('#network-selection').getElementsByTagName('li'));
+const selectionPanelText = document.querySelector("#select-network").querySelector('p');
+const netSelection = document.querySelector('#network-selection');
+let netSelectionOptions = Array.from(netSelection.getElementsByTagName('li'));
+const selectionClass = 'navbar-network-selected';
 netSelectionOptions.shift();
 
+const unsetPrevSelection = (element, arr) => {
+    const elementIndex = arr.indexOf(element);
+    arr.map(item => {
+        if (arr.indexOf(item) !== elementIndex 
+            && item.classList.contains(selectionClass)) {
+                item.classList.remove(selectionClass);
+            }   
+    });
+}
+
+let anchorTagsArr = Array.from(netSelection.querySelectorAll('a'));
+
 netSelectionOptions.map(li => {
-    li.addEventListener('click', () => li.querySelector('a').classList.add('navbar-network-selected'));
+    li.addEventListener('click', () => {
+        let anchorTag = li.querySelector('a');
+        
+        document.querySelector("#selection-icon").src = li.querySelector('img').src;
+        unsetPrevSelection(anchorTag, anchorTagsArr);
+        
+        anchorTag.classList.add(selectionClass);
+        selectionPanelText.textContent = anchorTag.textContent;
+        
+        netSelection.style.display = 'none';
+        setTimeout(() => netSelection.style.display = '', 100); 
+    });
 });
